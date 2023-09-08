f5fc694859cf62f3426631929ac44a46f8bbc29b
SYNAPSYS\dilshanw <dilshan.weerathunga@sysnapsys.lk>
2023-09-09 01:18:23 +0530

    created api for get student by name

--- src/main/java/com/studentWebApp/controller/StudentController.java
+++ src/main/java/com/studentWebApp/controller/StudentController.java
@@ -3,9 +3,14 @@ package com.studentWebApp.controller;
 import com.studentWebApp.entity.Student;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
 @CrossOrigin
 @RestController
 public class StudentController {
@@ -29,8 +34,26 @@ public class StudentController {
 
             studentList.add(new Student("Dilshan", "Weerathunga"));
             studentList.add(new Student("Tharaki", "Weththewa"));
+//            studentList.add(new Student("Nishada", "Dilshan"));
+//            studentList.add(new Student("AND", "Weththewa"));
 
 
         return  studentList;
+    }
+
+
+    @GetMapping("/getStudentByName/{studentName}")
+    public Map<String, Boolean> getStudentByName(@PathVariable String studentName){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("dilshan");
+        list.add("Nishada");
+        list.add("Tharaki");
+
+        if(list.contains(studentName)){
+            return Collections.singletonMap("status" , true);
+        }else{
+            return Collections.singletonMap("status" , false);
+        }
+
     }
 }
